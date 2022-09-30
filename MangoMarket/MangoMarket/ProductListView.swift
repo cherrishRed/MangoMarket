b13c9557284934cd2c2d12e82efcc47a30d157dd
Red <seryprk@gmail.com>
2022-10-01 01:18:47 +0900

    ✨feat : 이미지만 보기 액션 구현

--- MangoMarket/MangoMarket/ProductListView.swift
+++ MangoMarket/MangoMarket/ProductListView.swift
@@ -8,67 +8,91 @@
 import SwiftUI
 
 struct ProductListView: View {
+  @State var onlyImage: Bool = false
   var columns: [GridItem] = [GridItem(.flexible(), spacing: 10, alignment: nil),
                                     GridItem(.flexible(), spacing: 10, alignment: nil)]
   
   var body: some View {
     ScrollView {
-      LazyVGrid(columns: columns, alignment: .center, spacing: 10, pinnedViews: []) {
+      LazyVGrid(columns: columns, alignment: .center, spacing: 10, pinnedViews: [.sectionHeaders]) {
         
         Section(header:
-            ProductListHeaderView()
+            ProductListHeaderView(onlyImage: $onlyImage)
             ) {
           ForEach(1..<100) { number in
-            VStack(alignment: .leading) {
-              ZStack(alignment: .bottomTrailing) {
-                Rectangle()
-                  .scale(x: 1, y: 1, anchor: .center)
-                  .fill(.gray)
-                  .frame(maxWidth: .infinity)
-                  .aspectRatio(1, contentMode: .fill)
-                .cornerRadius(10)
-                Image(systemName: "heart")
-                  .foregroundColor(.red)
-                  .padding(.bottom, 10)
-                  .padding(.trailing, 10)
-              }
-              
-              VStack(alignment: .leading) {
-                Text("\(number) 번 제품")
-                  .font(.title3)
-                  .fontWeight(.medium)
-                Text("12000")
-                  .strikethrough()
-                  .foregroundColor(.gray)
-                HStack {
-                  Text("20%")
-                    .foregroundColor(.red)
-                  Text("9600")
-                }
-                Text("잔여수량 : 38 개")
-              }
-              .padding(.leading, 8)
+            ProductCellView(onlyImage: $onlyImage, product: number)
             }
           }
         }
-      }
       .padding()
+      }
     }
   }
-}
 
 struct ProductListHeaderView: View {
+  @Binding var onlyImage: Bool
+  
+  
   var body: some View {
     VStack(alignment: .leading) {
       HStack {
         Button {
-          
+          onlyImage.toggle()
         } label: {
-          Image(systemName: "square")
+          if onlyImage == true {
+            Image(systemName: "chevron.down.square")
+              .foregroundColor(.black)
+          } else {
+            Image(systemName: "square")
+              .foregroundColor(.black)
+          }
         }
         Text("이미지만 보기")
         Spacer()
       }
     }
+    .padding(.bottom, 4)
+    .background(.white)
   }
+}
+
+struct ProductCellView: View {
+  @Binding var onlyImage: Bool
+  var product: Int
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      ZStack(alignment: .bottomTrailing) {
+        Rectangle()
+          .scale(x: 1, y: 1, anchor: .center)
+          .fill(.gray)
+          .frame(maxWidth: .infinity)
+          .aspectRatio(1, contentMode: .fill)
+        .cornerRadius(10)
+        Image(systemName: "heart")
+          .foregroundColor(.red)
+          .padding(.bottom, 10)
+          .padding(.trailing, 10)
+      }
+      
+      if onlyImage == false {
+        VStack(alignment: .leading) {
+          Text("\(product) 번 제품")
+            .font(.title3)
+            .fontWeight(.medium)
+          Text("12000")
+            .strikethrough()
+            .foregroundColor(.gray)
+          HStack {
+            Text("20%")
+              .foregroundColor(.red)
+            Text("9600")
+          }
+          Text("잔여수량 : 38 개")
+        }
+        .padding(.leading, 8)
+      }
+  }
+}
+
 }

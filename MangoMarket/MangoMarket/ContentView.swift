7cd5d8e418698e57440acb4b1ed13ea90b672d43
Red <seryprk@gmail.com>
2022-09-30 19:45:19 +0900

    ✨feat : 새 프로젝트 생성

---
+++ MangoMarket/MangoMarket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MangoMarket
+//
+//  Created by RED on 2022/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MangoMarket/MangoMarket/MangoMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangoMarketApp.swift
+//  MangoMarket
+//
+//  Created by RED on 2022/09/30.
+//
+
+import SwiftUI
+
+@main
+struct MangoMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

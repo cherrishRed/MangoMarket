71291cfcfc72c431e34664c466fe2a84b0e48fb4
Red <seryprk@gmail.com>
2022-09-30 20:51:33 +0900

    ✨feat : 자동으로 움직이는 배너뷰로 수정

--- MangoMarket/MangoMarket/MainView.swift
+++ MangoMarket/MangoMarket/MainView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 struct MainView: View {
     var body: some View {
@@ -42,16 +43,31 @@ struct HeaderView: View {
 }
 
 struct BannerView: View {
-  @State var banners: [Color] = [.red, .yellow, .purple]
+  @State var selection: Int = 0
   
   var body: some View {
-    TabView{
-      ForEach(banners, id:\.self) { color in
-        Rectangle()
-          .fill(color)
-      }
+    TabView(selection: $selection){
+      Rectangle()
+        .fill(.red)
+        .tag(0)
+      Rectangle()
+        .fill(.yellow)
+        .tag(1)
+      Rectangle()
+        .fill(.purple)
+        .tag(2)
     }
+    .animation(.easeInOut, value: selection)
     .frame(maxWidth: .infinity, maxHeight: 200)
     .tabViewStyle(.page)
+    .onAppear {
+      Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+        if selection == 2 {
+          selection = 0
+        } else {
+          selection += 1
+        }
+      }
+    }
   }
 }

aff2dc5db0295de9467c7ccb5f2aa0d3dbc6768a
Red <seryprk@gmail.com>
2022-09-30 19:56:06 +0900

    ✨feat : ProductList Model 생성

---
+++ MangoMarket/MangoMarket/Model/ProductList.swift
@@ -0,0 +1,32 @@
+//
+//  ProductList.swift
+//  MangoMarket
+//
+//  Created by RED on 2022/09/30.
+//
+
+import Foundation
+
+struct ProductList: Codable {
+    let pageNumber: Int?
+    let itemsPerPage: Int?
+    let totalCount: Int?
+    let offset: Int?
+    let limit: Int?
+    let items: [ProductDetail]?
+    let lastPage: Int?
+    let hasNext: Bool?
+    let hasPrev: Bool?
+
+    private enum CodingKeys: String, CodingKey {
+        case pageNumber = "page_no"
+        case itemsPerPage = "items_per_page"
+        case totalCount = "total_count"
+        case offset
+        case limit
+        case items = "pages"
+        case lastPage = "last_page"
+        case hasNext = "has_next"
+        case hasPrev = "has_prev"
+    }
+}

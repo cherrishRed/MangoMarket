bf00d44054d30b5c026994c191f9eea376c34810
Red <seryprk@gmail.com>
2022-09-30 19:54:36 +0900

    ✨feat : ProductDetail Model 생성

---
+++ MangoMarket/MangoMarket/Model/ProductDetail.swift
@@ -0,0 +1,72 @@
+//
+//  ProductDetail.swift
+//  MangoMarket
+//
+//  Created by RED on 2022/09/30.
+//
+
+import Foundation
+
+struct ProductDetail: Codable, Hashable {
+    let id: Int?
+    let vendorID: Int?
+    let name: String?
+    let thumbnail: URL?
+    let currency: String?
+    let price: Double?
+    let description: String?
+    let bargainPrice: Double?
+    let discountedPrice: Double?
+    let stock: Int?
+    let createdAt: String?
+    let issuedAt: String?
+    let imageInfos: [ProductImage]?
+    let vendor: Vendor?
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case vendorID = "vendor_id"
+        case name
+        case thumbnail
+        case currency
+        case price
+        case description
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case stock
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+        case imageInfos = "images"
+        case vendor = "vendors"
+    }
+}
+
+struct ProductImage: Codable, Hashable {
+    let id: Int?
+    let url: URL?
+    let thumbnailURL: URL?
+    let isSuccess: Bool?
+    let issuedAt: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case url
+        case thumbnailURL = "thumbnail_url"
+        case isSuccess = "succeed"
+        case issuedAt = "issued_at"
+    }
+}
+
+struct Vendor: Codable, Hashable {
+    let name: String?
+    let id: Int?
+    let createdAt: String?
+    let issuedAt: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case id
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+    }
+}

d129ed46a565fcfb09a3334cd43a5c8354ac409e
Red <seryprk@gmail.com>
2022-09-30 19:56:56 +0900

    ✨feat : Currency Model 생성

---
+++ MangoMarket/MangoMarket/Model/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  MangoMarket
+//
+//  Created by RED on 2022/09/30.
+//
+
+import Foundation
+
+enum Currency: String, Codable {
+     case KRW = "KRW"
+     case USD = "USD"
+ }

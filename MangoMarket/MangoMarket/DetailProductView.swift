89d4fc72dbdcb21324032323a26076ce6639ac31
Red <seryprk@gmail.com>
2022-10-02 19:03:05 +0900

    ✨feat : DetailProductView 생성

---
+++ MangoMarket/MangoMarket/DetailProductView.swift
@@ -0,0 +1,14 @@
+//
+//  DetailProductView.swift
+//  MangoMarket
+//
+//  Created by RED on 2022/10/02.
+//
+
+import SwiftUI
+
+struct DetailProductView: View {
+  var body: some View {
+    Text("디테일뷰")
+  }
+}

5f704479b6cd3610870a26733ebb164abc0ceb69
Red <seryprk@gmail.com>
2022-10-18 19:33:17 +0900

    ✨feat : HTTPMethod enum 생성

---
+++ MangoMarket/MangoMarket/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  MangoMarket
+//
+//  Created by RED on 2022/10/18.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}

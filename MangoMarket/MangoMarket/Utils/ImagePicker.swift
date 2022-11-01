959d76a3fba0184af52743a7d548717450d44add
Red <seryprk@gmail.com>
2022-11-01 14:13:22 +0900

    ♻️refactor : image picker 사진 수정 가능하게 변경

--- MangoMarket/MangoMarket/Utils/ImagePicker.swift
+++ MangoMarket/MangoMarket/Utils/ImagePicker.swift
@@ -15,7 +15,7 @@ struct ImagePicker: UIViewControllerRepresentable {
   func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
     
     let imagePicker = UIImagePickerController()
-    imagePicker.allowsEditing = false
+    imagePicker.allowsEditing = true
     imagePicker.sourceType = sourceType
     imagePicker.delegate = context.coordinator
     
@@ -40,7 +40,7 @@ struct ImagePicker: UIViewControllerRepresentable {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
       
-      if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+      if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
         parent.selectedImage.append(image)
       }
       
